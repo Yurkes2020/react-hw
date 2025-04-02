a46473b884128e1f92236b7ad7deb19598a3628d
Yurkes2020 <yurkes2020@gmail.com>
2025-04-02 22:19:16 +0300

    hw 1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import {coursesAndDurationArray} from "./data/data.tsx";
 
 function App() {
-    return (<>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </>)
+	return (<>
+		{coursesAndDurationArray.map(({title, monthDuration}, i) => {
+			return (<div className="flex gap-2 justify-center" key={i}>
+				<h2 className="text-blue-300 ">{title}</h2>
+				<p className="text-green-300 ">{monthDuration} months</p>
+			</div>)
+		})}
+	</>)
 }
 
 export default App
