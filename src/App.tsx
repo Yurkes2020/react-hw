79a487c865a37a4e3524749307d0a2ae3ec1964d
Yurkes2020 <yurkes2020@gmail.com>
2025-04-09 21:27:37 +0300

    2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
+import {TodoComponents} from "./components/TodoComponents.tsx";
 
 function App() {
     return (<>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <TodoComponents/>
     </>)
 }
 
